68a07cffa875afda413eb3ffdcdc7804f5cb2c6c
wuyinsheng <wysheng97@qq.com>
2019-04-18 15:55:16 +0800

    创建项目 输出helloworld 再次输出helloworld
    Signed-off-by: wuyinsheng <wysheng97@qq.com>

--- src/cn/ysheng/HelloWorld.java
+++ src/cn/ysheng/HelloWorld.java
@@ -3,5 +3,6 @@ package cn.ysheng;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello,World!");
+        System.out.println("Hello,World!");
     }
 }
